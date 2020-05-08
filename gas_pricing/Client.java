32045e632b363a9c5ad0375b610f17be75a27926
Pedro Diógenes <pedroyolo@live.com>
2020-05-07 21:52:22 -0300

    added gas pricing with UDP datagram sockets

---
+++ gas_pricing/Client.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Client {
+    public InetAddress ip;
+    public int port;
+    public DatagramSocket clientSocket = null;
+    public DatagramPacket sendPacket = null;
+    public DatagramPacket receivPacket = null;
+
+    Client(InetAddress ip, int port) throws IOException, SocketException{
+        this.ip = ip;
+        this.port = port;
+        this.clientSocket = new DatagramSocket();
+        this.run();
+    }
+
+    void run() throws IOException, SocketException{
+        // create the clients socket
+        DatagramSocket clientSocket = new DatagramSocket();
+        BufferedReader clientRead = 
+            new BufferedReader(new InputStreamReader(System.in));
+        while(true){ // run until told otherwise
+
+            // create buffers to send and receive messages
+            byte[] sendBuffer = new byte[65535];
+            byte[] receiveBuffer = new byte[65535];
+
+            // sending data
+
+            System.out.println("\nType your message to the server following the instructions:");
+            System.out.println("Type 'd <type> <price x 1000> <latitude> <longitude>' to enter data");
+            System.out.println("Type 'p <type> <radius> <latitude> <longitude>' to receive data.");
+            System.out.println("Types: 0 - diesel 1 - alcohol 2 - gasoline");
+
+            String clientData = clientRead.readLine();
+            sendBuffer = clientData.getBytes();
+
+            DatagramPacket sendPacket = 
+                new DatagramPacket(sendBuffer, sendBuffer.length, this.ip, this.port);
+            
+            
+            try{
+                clientSocket.send(sendPacket);
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+
+            // receiving data
+
+            DatagramPacket receivePacket = 
+                new DatagramPacket(receiveBuffer, receiveBuffer.length);
+
+            clientSocket.receive(receivePacket);
+            String serverData = new String(receivePacket.getData());
+            System.out.println("\nServer says: " + serverData + "\n");
+
+            if(clientData.equalsIgnoreCase("bye")){
+                break;
+            }
+
+        }
+
+        clientSocket.close();
+    }
+
+    public static void main(String[] args)  throws IOException{
+        
+        // checks for correct usage
+        if(args.length != 2){
+            System.out.println("Usage: java Client <ip_address> <port>");
+            return;
+        }
+
+        InetAddress ip = null;
+
+        try{
+            ip = InetAddress.getByName(args[0]);
+        } catch (UnknownHostException e){
+            e.printStackTrace();
+        }
+        
+        int port = Integer.parseInt(args[1]);
+
+
+        Client client = new Client(ip, port);
+    }
+}
---
+++ gas_pricing/Server.java
@@ -0,0 +1,122 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Server {
+    int port;
+    DatagramSocket serverSocket = null;
+    DatagramPacket receivedPacket = null;
+    DatagramPacket sendPacket = null;
+
+    Server(int port) throws IOException, SocketException{
+        this.serverSocket = new DatagramSocket(port);
+        this.run();
+    }
+
+    void run() throws IOException, SocketException{
+        while(true){
+            byte[] receiveBuffer = new byte[65535];
+            byte[] sendBuffer = new byte[65535];
+    
+            this.receivedPacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+            serverSocket.receive(receivedPacket);
+    
+            InetAddress ip = receivedPacket.getAddress();
+            int sender_port = receivedPacket.getPort();
+    
+            String clientData = new String(receivedPacket.getData());
+            System.out.println("\nClient says: " + clientData + "\n");
+
+
+            String serverData = handle_message(clientData);
+            sendBuffer = serverData.getBytes();
+            this.sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, ip, sender_port);
+    
+            serverSocket.send(sendPacket);
+            if(serverData.equalsIgnoreCase("bye")){
+                break;
+            }
+
+            receiveBuffer = new byte[65535];
+            sendBuffer = new byte[65535];
+        }
+
+        serverSocket.close();
+    }
+
+    String handle_message(String clientData) throws IOException{
+        String[] data = clientData.split(" ");
+        if(data[0].equalsIgnoreCase("d")){
+            // receiving data to add to file.
+            FileWriter fw = new FileWriter("./data.txt",true);
+            String type = data[1].trim(), price = data[2].trim();
+            String lat = data[3].trim(), lon = data[4].trim();
+            fw.write(type + " " + price + " " + lat + " " + lon + "\n");
+            fw.close();
+
+            return ("\nData added to file\n");
+        } else if (data[0].equalsIgnoreCase("p")){
+            int type = Integer.parseInt(data[1].trim()), radius = Integer.parseInt(data[2].trim());
+            int lat = Integer.parseInt(data[3].trim()), lon = Integer.parseInt(data[4].trim());
+            return this.get_cheapest(type, radius, lat, lon);
+        } else return "Wrong input, follow the instructions.\n";
+    }
+
+    String get_cheapest(int type, int radius, int lon, int lat) throws IOException{
+        String str;
+        ArrayList<String> entries = new ArrayList<String>();
+
+        // reads strings from the file
+        BufferedReader bf = new BufferedReader(new FileReader("./data.txt"));
+        while((str = bf.readLine()) != null){
+            entries.add(str);
+        }
+
+        double min_d = 99999999;
+        int min_p = 99999999;
+        System.out.println("Calculating...");
+        for(String s : entries){
+            String entry[] = s.split(" ");
+            if(Integer.parseInt(entry[0]) == type){
+                System.out.println(s);
+                int price = Integer.parseInt(entry[1].trim());
+                int lat_entry = Integer.parseInt(entry[2].trim());
+                int lon_entry = Integer.parseInt(entry[3].trim());
+
+                double d = this.calc_dist(lat, lon, lat_entry, lon_entry);
+                System.out.println("Price: " + price + " Distance: " + d);
+                if(price < min_p && d < radius){
+                    min_d = d;
+                    min_p = price;
+                }
+
+            }
+        }
+
+        double final_p = (double)min_p / 1000;
+
+        return "Price: " + final_p + " Distance: " + min_d + "\n";
+
+    }
+
+    double calc_dist(int lat, int lon, int lat_entry, int lon_entry){
+        return Math.sqrt(
+                Math.pow((lon_entry - lon), 2) +
+                Math.pow((lat_entry - lat), 2)
+                );
+    }
+
+
+
+    public static void main(String[] args) throws IOException, SocketException{
+        // checks for correct usage
+        if(args.length != 1){
+            System.out.println("Usage: java Server <port>");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server server = new Server(port);
+    }
+}
